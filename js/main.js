69b3805f3c154b6b762dde257075dc1c5b0dd664
Eliyas Hossain <eliyashossain700@gmail.com>
2022-02-15 20:03:59 +0600

    Getting Buyer Input After Click Submit Button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 // Date Added
 const date = new Date();
 document.getElementById('show-date').innerText=date.toLocaleDateString();
+
+// SubmIt Button Click And Get Input Data
+const detailsSubmitButton = document.getElementById('detail-submit-btn');
+detailsSubmitButton.addEventListener('click', function(){
+    const buyerDetailsInput =document.getElementById('buyer-details-input');
+    const buyerInfo = document.getElementById('buyer-info');
+    buyerInfo.innerText = buyerDetailsInput.value;
+})
